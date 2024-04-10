8cd39cfba5e30e4ff88852add11d60e116d337df
miguelcoria94 <miguelcoria94@gmail.com>
2024-04-10 12:19:54 -0500

    addded sidebar menu item and started note model

---
+++ models/note.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// I want the note to have a user id that references the user who created the note.
+
+const noteSchema = new Schema(
+  {
+    title: String,
+    body: String,
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    aiSummary: String,
+    
+  },
+  {
+    timestamps: true,
+  }
+);
